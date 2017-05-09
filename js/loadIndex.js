e9cdc2b29194134d44280becce76d7a7c46060b1
hussamss <hussams@hotmail.ca>
2017-05-09 18:03:33 +0300

    This is to add the function of redirecting user from index2.html to weather.html imme., note the loadIndex.js contains another function which re-direct the user to weather.html upon click on the logo.

--- js/loadIndex.js
+++ js/loadIndex.js
@@ -1,16 +1,29 @@
+// This page is using the dynamic load; however, it is not working according to our desire. 
+// The second functionality is with 3 second delay. 
+// Onclick on the .getWeather image, delay and show the weather
+
 // $(function() {
-// 	var url = "index2.html";
-// 	$('#scene').show(3000).delay( 300 ).fadeIn( 3000 );
-// 	var url = "weather.html"
-// 	$('#wholePage').load(url + ' #wholePage').hide( 30 ).delay( 3000 ).fadeIn( 3000 );
+// 	$('#scene').click(function() {
+// 		var url = "weather.html"
+// 	$('#wholePage').load(url + ' #wholePage').hide().delay( 3000 ).fadeIn( 3000 );
+// 	});
+	
 // });
 
-// Onclick on the .getWeather image, delay and show the weather
-$(function() {
-	$('#scene').click(function() {
-		var url = "weather.html"
-	$('#wholePage').load(url + ' #wholePage').hide().delay( 3000 ).fadeIn( 3000 );
-	});
-	
-});
+// This one is onclick, clicking the getweather logo
 
+// $('#scene').click (function (e){
+//    e.preventDefault(); 
+
+//    setTimeout(function () {
+//        window.location.href = "weather.html"; //will redirect to your blog page (an ex: weather.html)
+//     }, 3000); //will call the function after 3 secs.
+
+// });
+
+// This function is to redirect you, once you enter the page. 
+$(function(){
+   setTimeout(function () {
+       window.location.href = "weather.html"; //will redirect to your blog page (an ex: weather.html)
+    }, 3000); //will call the function after 3 secs.
+});
